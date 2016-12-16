27ec3d06153eb449cc07b7954ceda5e631b6ee06
RayRaya <xic768210@yahoo.com.tw>
2016-12-16 21:12:12 +0800

    build rayray branch

--- SNA/wowoo/admin.py
+++ SNA/wowoo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import wowoo
+from .models import user
 
 # Register your models here.
-admin.site.register(wowoo)
+admin.site.register(user)
--- SNA/wowoo/models.py
+++ SNA/wowoo/models.py
@@ -3,6 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class wowoo(models.Model):
+class user(models.Model):
 
-	 user_id = models.CharField(max_length=50)
+	user_id = models.CharField(max_length=20)
+	user_email = models.CharField(max_length=50)
+	# user_photo = 
+
+	def __unicode__(self):
+		return self.user_id
