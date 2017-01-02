03b24624d089097606826d050a26f2652047587f
RayRaya <xic768210@yahoo.com.tw>
2017-01-02 23:52:22 +0800

    table and data problem

--- SNA/wowoo/models.py
+++ SNA/wowoo/models.py
@@ -4,24 +4,24 @@ from django.db import models
 
 # User data table.
 class wowoo(models.Model):
-    user_id = models.CharField(max_length=200,null = True,blank = True )
-    user_name = models.CharField(max_length=200,null=True,blank=True)
-    user_picture = models.URLField(max_length=200,null = True,blank = True)
-    user_email = models.EmailField(null = True,blank = True)
-    user_nickname = models.CharField(max_length=200,null=True,blank=True)
-    user_achievement = models.IntegerField(default=0)
+    # user_id = models.CharField(max_length=200,null = True,blank = True )
+    # user_name = models.CharField(max_length=200,null=True,blank=True)
+    # user_picture = models.URLField(max_length=200,null = True,blank = True)
+    # user_email = models.EmailField(null = True,blank = True)
+    # user_nickname = models.CharField(max_length=200,null=True,blank=True)
+    # user_achievement = models.IntegerField(default=0)
 	 
     def key(self):
         return "wowoo"
 
 # Post data table        
 class Post(models.Model):
-    post_title= models.CharField(max_length=50,null = True,blank = True )
-    post_question = models.CharField(max_length=200,null = True,blank = True)
-    post_content = models.CharField(max_length=200,null = True, blank = True)
-    post_date = models.DateTimeField(auto_now_add = True,auto_now = False)
-    post_emotions = models.PositiveIntegerField(default=0)
-    likes = models.PositiveIntegerField(default=0)
+    # post_title= models.CharField(max_length=50,null = True,blank = True )
+    # post_question = models.CharField(max_length=200,null = True,blank = True)
+    # post_content = models.CharField(max_length=200,null = True, blank = True)
+    # post_date = models.DateTimeField(auto_now_add = True,auto_now = False)
+    # post_emotions = models.PositiveIntegerField(default=0)
+    # likes = models.PositiveIntegerField(default=0)
     
     def post(self):
         return "post"
